8a93ef1bec871b38e3e8a41f11f7c21eca1e7f11
piyapangithub <105621720+piyapangithub@users.noreply.github.com>
2022-07-24 13:41:11 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,34 @@
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const chrome = require("selenium-webdriver/chrome");
+const { Builder, By, Key, until } = require("selenium-webdriver");
 
-(async function example() {
-    let driver = new webdriver.Builder()
-    .forBrowser(webdriver.Browser.Chrome)
-    //.usingServer('http://localhost:4444/wd/hub')
+(async function googleSearch() {
+  let driver = await new Builder()
+    .forBrowser("chrome")
+    .usingServer("http://34.142.189.109:4444/wd/hub/")
+    .setChromeService(
+      chrome.setDefaultService(
+        new chrome.ServiceBuilder("./drivers/chromedriver").build()
+      )
+    )
     .build();
+
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    // Navigate to Url
+    await driver.get("https://www.google.com");
+    // Enter text "Automation Bro" and perform keyboard action "Enter"
+    await driver
+      .findElement(By.name("q"))
+      .sendKeys("Automation Bro", Key.ENTER);
+
+    let firstResult = await driver.wait(
+      until.elementLocated(By.css("h3")),
+      10000
+    );
+
+    console.log(await firstResult.getAttribute("textContent"));
+    console.log(await (await driver.getCapabilities()).getBrowserName());
+    console.log(await (await driver.getCapabilities()).getBrowserVersion());
   } finally {
-    await driver.quit();
+    driver.quit();
   }
 })();
